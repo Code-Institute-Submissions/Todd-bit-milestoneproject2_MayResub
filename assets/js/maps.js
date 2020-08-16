006b1a36165f4b62fa2d049fcce62cd0bd1ca9ee
Todd-bit <todd7385@gmail.com>
2020-08-16 20:23:40 +0000

    Did map work

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,18 +1,21 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 10,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 40.5187,
+            lng: -78.3947
         }
     });
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     var locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 }
+        { lat: 40.513631, lng: -78.399484 },
+        { lat: 40.497411, lng: -78.483745 },
+        { lat: 40.582704, lng: -78.273789 },
+        { lat: 40.490108, lng: -78.408588 },
+        { lat: 40.451305, lng: -78.549257 },
+        { lat: 40.482815, lng: -78.551774 }
     ];
 
     var markers = locations.map(function(location, i) {
